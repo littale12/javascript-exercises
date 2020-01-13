a086c190944f0810e4cce0acd870c6ff7e6178ca
littale12 <littale12@gmail.com>
2020-01-12 22:32:02 -0500

    completed helloworld, getthetitles

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,21 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
+let element = []
+
+const getTheTitles = function() {
+    for (let i = 0; i < books.length; i++) {
+        element.push(books[i].title);
+    }
+    return element
 }
 
 module.exports = getTheTitles;
--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
 const removeFromArray = function() {
-
-}
+    for (let i = 0; i < arguments[0].length; i++) {
+        if (arguments[1..arguments.length].includes(arguments[0][i]) == true) {
+            arguments[0].splice(i,1)
+        };
+    };
+    return arguments[0]
+};
 
 module.exports = removeFromArray
