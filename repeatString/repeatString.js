7555b48b03a8ea4a33c64404d9efe7d3e3859996
littale12 <littale12@gmail.com>
2020-01-16 16:56:52 -0500

    completed repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let element = ""
+    if (num < 0) {
+        return "ERROR"
+    } else if (num == 0) {
+        return ""
+    }
+    for (let i = 0; i < num; i++) {
+        element += `${string}`;
+    }
+    return element
 }
-
 module.exports = repeatString
