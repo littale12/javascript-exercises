28567d12cde514656b89d676cf31ef655476f452
littale12 <littale12@gmail.com>
2020-01-16 00:15:16 -0500

    banged my head against the wall for 4 hours before reading xit documentation. always read documentation. removeFromArray complete

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,13 +1,15 @@
-const removeFromArray = function(...args) {
-    let array = args[0]
-    let rem = args.slice(1)
-    for (let i = 0; i < args[0].length; i++) {
+const removeFromArray = function() {
+    const args = [].slice.call(arguments)
+    let array = args[0].sort()
+    const rem = args.slice(1).sort()
+    for (let i = 0; i < array.length; i++) {
         if (rem.includes(array[i])) {
-            array.splice(i,1)
-        };
-    };
+            array.splice(i, 1)
+            i--
+        }
+    }
     return array
-};
+}
 
 
 
