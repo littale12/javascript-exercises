ca004308bf67058cc8d3cf5187b21f889f1b70c8
littale12 <littale12@gmail.com>
2020-01-16 00:49:17 -0500

    started caesar

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,16 @@
-const caesar = function() {
-
+const caesar = function(string, num) {
+    const caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const lower = "abcdefghijklmnopqrstuvwxyz"
+    let finalString = ""
+    for (let i = 0; i < string.length; i++) {
+        if (lower.includes(string[i])) {
+            finalString += lower[Math.abs(lower.indexOf(string[i])+num)%26]
+        } else if (caps.includes(string[i])) {
+            finalString += caps[Math.abs(caps.indexOf(string[i])+num)%26]
+        } else
+            finalString += string[i]
+    }
+    return finalString
 }
 
 module.exports = caesar
