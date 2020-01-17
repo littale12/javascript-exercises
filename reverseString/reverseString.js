ea9abc53aa14f666e7668f52c55b36e1315fcbc8
littale12 <littale12@gmail.com>
2020-01-17 10:06:42 -0500

    completed reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
-}
+const reverseString = function(string) {
+    let switchArray = []
+    let outputString = ""
+    for (let i = 0; i < string.length; i++) {
+        switchArray.push(string[i]);
+    }
+    switchArray = switchArray.reverse()
+    for (let i = 0; i < string.length; i++) {
+        outputString += switchArray[i];
+    }
+    return outputString
+}   
 
 module.exports = reverseString
