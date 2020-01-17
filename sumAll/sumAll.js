7b56fb1f765b0583dab9aba33909285a773f7b9d
littale12 <littale12@gmail.com>
2020-01-17 10:28:58 -0500

    completed sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(beg, end) {
+    let sum = 0
+    if (end < 0 || beg < 0 || typeof beg !== 'number' || typeof end !== 'number') {
+        return "ERROR"
+    }
+    if (end > beg) {
+        for (let i = beg; i <= end; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = end; i <= beg; i++) {
+            sum += i;
+        }
+    }
+    return sum
 }
 
 module.exports = sumAll
