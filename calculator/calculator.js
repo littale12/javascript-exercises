61ffba1970e259f0d09838077e52e231998d99dd
littale12 <littale12@gmail.com>
2020-01-23 16:12:34 -0500

    finished this one off pretty quickly. making some good progress

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,26 +1,31 @@
 function add () {
-	
+	return arguments[0] + arguments[1];
 }
 
-function subtract () {
-	
+function subtract (num1, num2) {
+	return num1 - num2;
 }
 
-function sum () {
-	
+function sum (numList) {
+	return numList.reduce((sum, num) => {
+		return sum + num;
+	}, 0);
 }
 
-function multiply () {
-	
+function multiply (numList) {
+	return numList.reduce((total, num) => {
+		return total * num;
+	}, 1);
 }
 
-function power() {
-	
+function power(num, pow) {
+	return num ** pow
 }
 
-function factorial() {
-	
+function factorial(n) {
+	return (n != 1 && n != 0) ? n * factorial(n-1) : 1;
 }
+// i may or may not have sought outside help for this last one
 
 module.exports = {
 	add,
