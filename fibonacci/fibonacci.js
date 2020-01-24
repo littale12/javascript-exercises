62df408ee99bad2bb642d4d1c032b21df04c5a10
littale12 <littale12@gmail.com>
2020-01-23 23:23:39 -0500

    solved with furst homemade recursive function

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const fibNum = Number(num);
+    if (fibNum < 0) {return "OOPS"};
+    return (fibNum > 2) ? fibonacci(fibNum-1) + fibonacci(fibNum-2) : 1;
 }
+
+//first recursive function i didn't have to google
 
 module.exports = fibonacci
--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(leapYear) {
+    if (leapYear % 400 % 4) {
+        return true
+    }
+    return false
 }
 
 module.exports = leapYears
