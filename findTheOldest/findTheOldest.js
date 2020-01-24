b1d10f84f30127b5e5695d4e8f41d371d12f3b0b
littale12 <littale12@gmail.com>
2020-01-24 00:31:40 -0500

    completed findTheOldest after some hard reading and head scratching

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
 let findTheOldest = function() {
-
+    return people.reduce((oldest, current) => {
+        const oldestAge = age(oldest.yearOfDeath, oldest.yearOfBirth)
+        const currentAge = age(current.yearOfDeath, current.yearOfBirth)
+        return oldestAge < currentAge ? current : oldest
+        //important to keep in mind that this return just overwrites the current value of the oldest var
+    })
 }
 
+const age = function(death, birth) {
+    if (!death) {
+       death = new Date().getFullYear()
+    }
+    return death - birth
+}
 module.exports = findTheOldest
