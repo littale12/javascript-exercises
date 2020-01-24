71551b91bef51ccd1d7e100a53fc5abc9921e43a
littale12 <littale12@gmail.com>
2020-01-23 22:37:55 -0500

    took a lot of tinkering but i solved this one

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function() {
-
+const palindromes = function(str) {
+    let delmtedStr = str.split(",").join("").split(" ").join("").toLowerCase()
+    const pld1 = delmtedStr.split("").reverse().join("")
+    const pld2 = delmtedStr.substring(0, delmtedStr.length - 1).split("").reverse().join("")
+    return (pld1 == delmtedStr || pld2 == delmtedStr.substring(0, delmtedStr.length - 1)) ? true : false;
 }
-
+// this just feels unnecessarily ugly but it works so i'll take it
 module.exports = palindromes
